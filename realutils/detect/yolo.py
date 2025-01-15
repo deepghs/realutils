1fc391200586e4961ea44d1133a21da2c11e3067
narugo1992 <narugo1992@deepghs.org>
2025-01-15 19:56:03 +0800

    dev(narugo): add examples of yolo models

--- realutils/detect/yolo.py
+++ realutils/detect/yolo.py
@@ -51,6 +51,18 @@ def detect_by_yolo(image: ImageTyping, model_name: str = _DEFAULT_MODEL,
 
     :raises ValueError: If an invalid level is provided.
     :raises RuntimeError: If the model fails to load or process the image.
+
+    :exmple:
+        >>> from realutils.detect import detect_by_yolo
+        >>>
+        >>> detect_by_yolo('yolo/unsplash_aJafJ0sLo6o.jpg')
+        [((450, 317, 567, 599), 'person', 0.9004617929458618)]
+        >>> detect_by_yolo('yolo/unsplash_n4qQGOBgI7U.jpg')
+        [((73, 101, 365, 409), 'vase', 0.9098997116088867), ((441, 215, 659, 428), 'vase', 0.622944176197052), ((5, 1, 428, 377), 'potted plant', 0.5178268551826477)]
+        >>> detect_by_yolo('yolo/unsplash_vUNQaTtZeOo.jpg')
+        [((381, 103, 676, 448), 'bird', 0.9061452150344849)]
+        >>> detect_by_yolo('yolo/unsplash_YZOqXWF_9pk.jpg')
+        [((315, 100, 690, 532), 'horse', 0.9453459978103638), ((198, 181, 291, 256), 'horse', 0.917123556137085), ((145, 173, 180, 249), 'horse', 0.7972317337989807), ((660, 138, 701, 170), 'horse', 0.4843617379665375)]
     """
     return yolo_predict(
         image=image,
