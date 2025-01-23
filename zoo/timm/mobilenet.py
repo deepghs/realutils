5d986ccbbbd7e605d56166fc5ed493b9da8e084f
narugo1992 <narugo1992@deepghs.org>
2025-01-24 00:25:11 +0800

    dev(narugo): add timm, ci skip

--- zoo/timm/mobilenet.py
+++ zoo/timm/mobilenet.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     logging.try_init_root(level=logging.INFO)
     sync(
         repository='deepghs/timms_mobilenet',
-        params_limit=0.03 * 1000 ** 3,
+        params_limit=0.03  * 1000 ** 3,
         max_count=100,
         name_filter=lambda x: 'mobilenet' in x,
     )
