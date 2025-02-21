bac4471b82526cbde5417a4a40df0196903f480e
narugo1992 <117186571+narugo1992@users.noreply.github.com>
2025-02-22 02:35:04 +0800

    Update meta.py

--- realutils/config/meta.py
+++ realutils/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'realutils'
 
 #: Version of this project.
-__VERSION__ = '0.0.3'
+__VERSION__ = '0.0.4'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A convenient and user-friendly image data processing library that integrates ' \
