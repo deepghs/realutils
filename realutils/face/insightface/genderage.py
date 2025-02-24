c2bb31e68660dd71bcac69092833c1705b265e75
narugo1992 <narugo1992@deepghs.org>
2025-02-24 19:29:58 +0800

    dev(narugo): add docstrings

--- realutils/face/insightface/genderage.py
+++ realutils/face/insightface/genderage.py
@@ -1,3 +1,10 @@
+"""
+This module provides functionality for gender and age detection in facial images using ONNX models.
+It includes functions for loading attribute detection models and performing gender/age inference on faces.
+
+The module integrates with Hugging Face's model hub and provides caching capabilities for model loading.
+"""
+
 from typing import Union, Tuple
 
 import numpy as np
@@ -10,6 +17,19 @@ from .base import _REPO_ID, _DEFAULT_MODEL, transform, Face
 
 @ts_lru_cache()
 def _open_attribute_model(model_name: str):
+    """
+    Load and cache an ONNX model for facial attribute detection.
+
+    :param model_name: Name of the model to load
+    :type model_name: str
+
+    :return: A tuple containing:
+        - ONNX session object
+        - Input tensor name
+        - Input shape tuple (height, width)
+        - List of output tensor names
+    :rtype: tuple[onnxruntime.InferenceSession, str, tuple[int, int], list[str]]
+    """
     session = open_onnx_model(hf_hub_download(
         repo_id=_REPO_ID,
         repo_type='model',
@@ -24,6 +44,24 @@ def _open_attribute_model(model_name: str):
 
 def isf_genderage(image: ImageTyping, face: Union[Face, Tuple[float, float, float, float]],
                   model_name: str = _DEFAULT_MODEL, no_write: bool = False):
+    """
+    Detect gender and age from a facial image.
+
+    This function performs gender and age detection on a given face in an image. It can work with
+    either a Face object or raw bounding box coordinates.
+
+    :param image: Input image (can be path, URL, PIL Image, or numpy array)
+    :type image: ImageTyping
+    :param face: Face object or bounding box coordinates (x1, y1, x2, y2)
+    :type face: Union[Face, Tuple[float, float, float, float]]
+    :param model_name: Name of the model to use for detection, defaults to _DEFAULT_MODEL
+    :type model_name: str
+    :param no_write: If True, don't update the Face object with results (if face is Face object)
+    :type no_write: bool
+
+    :return: Tuple of (gender, age) where gender is 'F' or 'M' and age is an integer
+    :rtype: tuple[str, int]
+    """
     if isinstance(face, Face):
         bbox = face.bbox
     else:
@@ -39,7 +77,7 @@ def isf_genderage(image: ImageTyping, face: Union[Face, Tuple[float, float, floa
     aimg, _ = transform(np.array(image), center, input_size, scale, 0)
 
     blob = aimg.astype(np.float32)
-    blob = blob.transpose(2, 0, 1)[np.newaxis]  # NCHW格式
+    blob = blob.transpose(2, 0, 1)[np.newaxis]  # NCHW format
 
     pred = session.run(None, {session.get_inputs()[0].name: blob})[0][0]
     gender = ['F', 'M'][np.argmax(pred[:2]).item()]
@@ -49,7 +87,3 @@ def isf_genderage(image: ImageTyping, face: Union[Face, Tuple[float, float, floa
         face.gender = gender
 
     return gender, age
-
-
-if __name__ == '__main__':
-    print(_open_attribute_model(_DEFAULT_MODEL))
