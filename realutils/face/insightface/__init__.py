01a79b6752b4ffc1af4b2452ff3ee07a0b4e35fd
narugo1992 <narugo1992@deepghs.org>
2025-02-25 23:26:13 +0800

    dev(narugo): add top level README

--- realutils/face/insightface/__init__.py
+++ realutils/face/insightface/__init__.py
@@ -1,3 +1,10 @@
+"""
+This module provides a comprehensive interface to InsightFace, a 2D/3D face analysis toolkit.
+It includes functionalities for face detection, recognition, gender & age estimation, and visualization.
+
+The models are hosted on Hugging Face Hub at `deepghs/insightface <https://huggingface.co/deepghs/insightface>`_,
+original project at `deepinsight/insightface <https://github.com/deepinsight/insightface>`_.
+"""
 from .analysis import isf_analysis_faces
 from .base import Face
 from .detect import isf_detect_faces
