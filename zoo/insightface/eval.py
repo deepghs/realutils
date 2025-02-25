2bcc6e86dbc9d666edb248c5855433e93217d5e4
narugo1992 <narugo1992@deepghs.org>
2025-02-25 15:30:57 +0800

    dev(narugo): add insight zoo

--- zoo/insightface/eval.py
+++ zoo/insightface/eval.py
@@ -4,10 +4,12 @@ import os
 from contextlib import contextmanager
 
 import numpy as np
+import pandas as pd
 from PIL import Image
 from ditk import logging
 from hbutils.system import TemporaryDirectory
 from hfutils.archive import archive_unpack
+from hfutils.operate import upload_directory_as_directory, get_hf_client
 from huggingface_hub import hf_hub_download
 from imgutils.data import load_image
 from tqdm import tqdm
@@ -38,8 +40,17 @@ def mock_eval_dataset(dsname: str):
         yield td
 
 
-def make_eval_result(model_name: str, rescale_ratio: float = 7):
+def make_eval_result(repo_id: str = 'deepghs/insightface', model_name: str = 'buffalo_l', rescale_ratio: float = 7):
+    hf_client = get_hf_client()
     for dsname in _AVAILABLE_DS:
+        if hf_client.file_exists(
+                repo_id=repo_id,
+                repo_type='model',
+                filename=f'{model_name}/{dsname}_sims.csv',
+        ):
+            logging.warn(f'Result for {dsname!r} already exist, skipped.')
+            continue
+
         logging.info(f'Eval {model_name!r} with dataset {dsname!r} ...')
         with mock_eval_dataset(dsname) as ds_dir:
             for image_file in tqdm(glob.glob(os.path.join(ds_dir, 'images', '**', '*'), recursive=True),
@@ -74,12 +85,39 @@ def make_eval_result(model_name: str, rescale_ratio: float = 7):
                 dst_emb_file = os.path.splitext(image_file)[0] + '.npy'
                 np.save(dst_emb_file, embedding)
 
-        quit()
+            df_src = pd.read_csv(os.path.join(ds_dir, 'paris.csv'))
+            records = []
+            for record in tqdm(df_src.to_dict('records'), desc='Calculate values'):
+                src_file1 = os.path.join(ds_dir, record['file1'])
+                npy_file1 = os.path.splitext(src_file1)[0] + '.npy'
+                src_file2 = os.path.join(ds_dir, record['file2'])
+                npy_file2 = os.path.splitext(src_file2)[0] + '.npy'
+                emb1, emb2 = np.load(npy_file1), np.load(npy_file2)
+                emb1 = emb1 / np.linalg.norm(emb1)
+                emb2 = emb2 / np.linalg.norm(emb2)
+                cosine = (emb1 * emb2).sum().item()
+                records.append({
+                    **record,
+                    'cos_sim': cosine,
+                })
+
+            df = pd.DataFrame(records)
+            logging.info(f'Results:\n{df}')
+
+            with TemporaryDirectory() as upload_dir:
+                df.to_csv(os.path.join(upload_dir, f'{dsname}_sims.csv'), index=False)
+                upload_directory_as_directory(
+                    repo_id=repo_id,
+                    repo_type='model',
+                    path_in_repo=model_name,
+                    local_directory=upload_dir,
+                    message=f'Add cosine result of dataset {dsname!r} for model {model_name!r}',
+                )
 
 
 if __name__ == '__main__':
     logging.try_init_root(level=logging.INFO)
-    make_eval_result('buffalo_l')
+    make_eval_result(model_name='buffalo_l')
     # with mock_eval_dataset('cfpw') as d:
     #     print(d)
     #     os.system(f'ls -al {d!r}')
